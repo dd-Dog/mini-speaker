50d5bdf64aad221bd1a4f726ce89025e487eefc2
gaohq <gaohq@flyscale.cn>
2020-06-29 15:12:14 +0800

    增加子线程
    Change-Id: I92b162ea6498f19a64e50c818c848587196b73d0

--- app/src/main/java/com/flyscale/alertor/receivers/RemoteControlReceiver.java
+++ app/src/main/java/com/flyscale/alertor/receivers/RemoteControlReceiver.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import com.flyscale.alertor.data.base.BaseData;
 import com.flyscale.alertor.data.persist.PersistConfig;
 import com.flyscale.alertor.data.up.UDoorAlarm;
+import com.flyscale.alertor.helper.ThreadPool;
 import com.flyscale.alertor.netty.AlarmHelper;
 import com.flyscale.alertor.netty.CallAlarmHelper;
 import com.flyscale.alertor.netty.NettyHelper;
@@ -26,44 +27,50 @@ public class RemoteControlReceiver extends BroadcastReceiver {
 
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        if(TextUtils.equals(intent.getAction(),ACTION)){
-            String status = intent.getStringExtra("status");
-            //A: 0100
-            //B: 0010
-            //C: 0001
-            //D: 1000
-            //门磁：0011
-            //红外：0101
-            //烟感：1001
-            //气感：1011
-            //按键和传感器的8中状态
-            Log.i(TAG, "onReceive: " + status);
-            if(status.equals("0001")){
-                //可以报警
-                PersistConfig.saveCanAlarm(true);
-            }else if(status.equals("1000")){
-                //不可以报警
-                PersistConfig.saveCanAlarm(false);
-            }
+    public void onReceive(Context context, final Intent intent) {
+        ThreadPool.getInstance().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(TextUtils.equals(intent.getAction(),ACTION)){
+                    String status = intent.getStringExtra("status");
+                    //A: 0100
+                    //B: 0010
+                    //C: 0001
+                    //D: 1000
+                    //门磁：0011
+                    //红外：0101
+                    //烟感：1001
+                    //气感：1011
+                    //按键和传感器的8中状态
+                    Log.i(TAG, "onReceive: " + status);
+                    if(status.equals("0001")){
+                        //可以报警
+                        PersistConfig.saveCanAlarm(true);
+                    }else if(status.equals("1000")){
+                        //不可以报警
+                        PersistConfig.saveCanAlarm(false);
+                    }
 
-            if(PersistConfig.findConfig().isCanAlarm()){
-                if(status.equals("0100")){
-                    AlarmHelper.getInstance().polling(null);
-                }else if(status.equals("0010")){
-                    CallAlarmHelper.getInstance().polling(null,true);
-                }else if(status.equals("0011")){
-                    AlarmHelper.getInstance().polling(null,BaseData.TYPE_DOOR_ALARM_U);
-                }else if(status.equals("0101")){
-                    //红外报警
-                    //等待需求
-                    //周工说不管了
-                }else if(status.equals("1001")){
-                    AlarmHelper.getInstance().polling(null,BaseData.TYPE_SMOKE_ALARM_U);
-                }else if(status.equals("1011")){
-                    AlarmHelper.getInstance().polling(null,BaseData.TYPE_GAS_ALARM_U);
+                    if(PersistConfig.findConfig().isCanAlarm()){
+                        if(status.equals("0100")){
+                            AlarmHelper.getInstance().polling(null);
+                        }else if(status.equals("0010")){
+                            CallAlarmHelper.getInstance().polling(null,true);
+                        }else if(status.equals("0011")){
+                            AlarmHelper.getInstance().polling(null,BaseData.TYPE_DOOR_ALARM_U);
+                        }else if(status.equals("0101")){
+                            //红外报警
+                            //等待需求
+                            //周工说不管了
+                        }else if(status.equals("1001")){
+                            AlarmHelper.getInstance().polling(null,BaseData.TYPE_SMOKE_ALARM_U);
+                        }else if(status.equals("1011")){
+                            AlarmHelper.getInstance().polling(null,BaseData.TYPE_GAS_ALARM_U);
+                        }
+                    }
                 }
             }
-        }
+        });
+
     }
 }
