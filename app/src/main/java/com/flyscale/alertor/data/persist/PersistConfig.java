a51e655ffb73903ebb7a8afea7c609e11e9d2dd5
bianjb <bianjb@flyscale.cn>
2021-02-02 16:14:27 +0800

    添加小喇叭FTP,TCP数据库字段
    Change-Id: I437216faf5a17d238529b28428a0ca322b570260

--- app/src/main/java/com/flyscale/alertor/data/persist/PersistConfig.java
+++ app/src/main/java/com/flyscale/alertor/data/persist/PersistConfig.java
@@ -1,6 +1,7 @@
 package com.flyscale.alertor.data.persist;
 
 import android.text.TextUtils;
+
 import com.flyscale.alertor.helper.DateHelper;
 
 import org.litepal.LitePal;
@@ -33,6 +34,102 @@ public class PersistConfig extends LitePalSupport {
     //报警灯常亮是否取反的标志  true 维持现状 false 取反
     boolean isDefaultAlarmOn = true;
 
+    /*bianjb-服务器地址，端口信息--开始*/
+    /******************飞图FTP测试服务器*******************/
+    String ftpHostNameDebugFly = "";
+    String ftpHostIPDebugFly = "192.168.1.104";
+    int ftpHostPortDebugFly = 22;
+    String ftpUsernameFly = "flyscale";
+    String ftpPasswordFly = "fly123";
+
+    /******************客户测试服务器*******************/
+    String ftpHostNameDebug = "ftp3.xjxlb.com";
+    String ftpHostIPDebug = "222.82.238.168";
+    int ftpHostPortDebug = 13001;
+    String ftpUsernameDebug = "";
+    String ftpPasswordDebug = "";
+
+    String tcpHostNameDebug1 = "xlb1.xjxlb.com";//两个域名使用同一IP，默认使用1
+    String tcpHostNameDebug2 = "xlb1.xj-ict.com";
+    String tcpHostIPDebug = "202.100.190.107";
+    int tcpPortDebug = 58005;
+
+    /******************客户正式服务器*******************/
+    String ftpHostNameRelease = "ftp1.xjxlb.com";
+    String ftpHostIPRelease = "202.100.190.107";
+    int ftpHostPortRelease = 58000;
+    String ftpUsernameRelease = "";
+    String ftpPasswordRelease = "";
+
+    String tcpHostNameRelease1 = "xlb3.xjxlb.com";//两个域名使用同一IP，默认使用1
+    String tcpHostNameRelease2 = "xlb3.xj-ict.com";
+    String tcpHostIPRelease = "47.104.3.240";
+    int tcpPortRelease = 58005;
+
+    public static PersistConfig saveFtpHostNameRelease(String hostName) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setFtpHostNameRelease(hostName);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveFtpHostIPRelease(String ip) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setFtpHostIPRelease(ip);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveFtpHostPortRelease(int port) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setFtpHostPortRelease(port);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveFtpUsernameRelease(String username) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setFtpUsernameRelease(username);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveFtpPasswordRelease(String password) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setFtpPasswordRelease(password);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveTcpHostNameRelease1(String tcpHostname) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setTcpHostNameRelease1(tcpHostname);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveTcpHostNameRelease2(String tcpHostname) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setTcpHostNameRelease2(tcpHostname);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveTcpHostIPRelease(String tcpHostIP) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setTcpHostIPRelease(tcpHostIP);
+        persistConfig.save();
+        return persistConfig;
+    }
+
+    public static PersistConfig saveTcpPortRelease(int tcpPort) {
+        PersistConfig persistConfig = findConfig();
+        persistConfig.setTcpPortRelease(tcpPort);
+        persistConfig.save();
+        return persistConfig;
+    }
+    /*bianjb-服务器地址，端口信息--结束*/
+
     public boolean isDefaultAlarmOn() {
         return isDefaultAlarmOn;
     }
@@ -42,11 +139,218 @@ public class PersistConfig extends LitePalSupport {
     }
 
     public boolean isAlarmOn() {
-        if(isDefaultAlarmOn){
+        if (isDefaultAlarmOn) {
             return isAlarmOn;
-        }else {
+        } else {
             return !isAlarmOn;
         }
+    }
+
+    public String getTcpHostNameDebug1() {
+        return tcpHostNameDebug1;
+    }
+
+    public PersistConfig setTcpHostNameDebug1(String tcpHostNameDebug1) {
+        this.tcpHostNameDebug1 = tcpHostNameDebug1;
+        return this;
+    }
+
+    public String getTcpHostNameDebug2() {
+        return tcpHostNameDebug2;
+    }
+
+    public PersistConfig setTcpHostNameDebug2(String tcpHostNameDebug2) {
+        this.tcpHostNameDebug2 = tcpHostNameDebug2;
+        return this;
+    }
+
+    public String getTcpHostIPDebug() {
+        return tcpHostIPDebug;
+    }
+
+    public PersistConfig setTcpHostIPDebug(String tcpHostIPDebug) {
+        this.tcpHostIPDebug = tcpHostIPDebug;
+        return this;
+    }
+
+    public int getTcpPortDebug() {
+        return tcpPortDebug;
+    }
+
+    public PersistConfig setTcpPortDebug(int tcpPortDebug) {
+        this.tcpPortDebug = tcpPortDebug;
+        return this;
+    }
+
+    public String getTcpHostNameRelease1() {
+        return tcpHostNameRelease1;
+    }
+
+    public PersistConfig setTcpHostNameRelease1(String tcpHostNameRelease1) {
+        this.tcpHostNameRelease1 = tcpHostNameRelease1;
+        return this;
+    }
+
+    public String getTcpHostNameRelease2() {
+        return tcpHostNameRelease2;
+    }
+
+    public PersistConfig setTcpHostNameRelease2(String tcpHostNameRelease2) {
+        this.tcpHostNameRelease2 = tcpHostNameRelease2;
+        return this;
+    }
+
+    public String getTcpHostIPRelease() {
+        return tcpHostIPRelease;
+    }
+
+    public PersistConfig setTcpHostIPRelease(String tcpHostIPRelease) {
+        this.tcpHostIPRelease = tcpHostIPRelease;
+        return this;
+    }
+
+    public int getTcpPortRelease() {
+        return tcpPortRelease;
+    }
+
+    public PersistConfig setTcpPortRelease(int tcpPortRelease) {
+        this.tcpPortRelease = tcpPortRelease;
+        return this;
+    }
+
+    public String getFtpHostNameDebugFly() {
+        return ftpHostNameDebugFly;
+    }
+
+    public PersistConfig setFtpHostNameDebugFly(String ftpHostNameDebugFly) {
+        this.ftpHostNameDebugFly = ftpHostNameDebugFly;
+        return this;
+    }
+
+    public String getFtpHostIPDebugFly() {
+        return ftpHostIPDebugFly;
+    }
+
+    public PersistConfig setFtpHostIPDebugFly(String ftpHostIPDebugFly) {
+        this.ftpHostIPDebugFly = ftpHostIPDebugFly;
+        return this;
+    }
+
+    public int getFtpHostPortDebugFly() {
+        return ftpHostPortDebugFly;
+    }
+
+    public PersistConfig setFtpHostPortDebugFly(int ftpHostPortDebugFly) {
+        this.ftpHostPortDebugFly = ftpHostPortDebugFly;
+        return this;
+    }
+
+    public String getFtpUsernameFly() {
+        return ftpUsernameFly;
+    }
+
+    public PersistConfig setFtpUsernameFly(String ftpUsernameFly) {
+        this.ftpUsernameFly = ftpUsernameFly;
+        return this;
+    }
+
+    public String getFtpPasswordFly() {
+        return ftpPasswordFly;
+    }
+
+    public PersistConfig setFtpPasswordFly(String ftpPasswordFly) {
+        this.ftpPasswordFly = ftpPasswordFly;
+        return this;
+    }
+
+    public String getFtpHostNameDebug() {
+        return ftpHostNameDebug;
+    }
+
+    public PersistConfig setFtpHostNameDebug(String ftpHostNameDebug) {
+        this.ftpHostNameDebug = ftpHostNameDebug;
+        return this;
+    }
+
+    public String getFtpHostIPDebug() {
+        return ftpHostIPDebug;
+    }
+
+    public PersistConfig setFtpHostIPDebug(String ftpHostIPDebug) {
+        this.ftpHostIPDebug = ftpHostIPDebug;
+        return this;
+    }
+
+    public int getFtpHostPortDebug() {
+        return ftpHostPortDebug;
+    }
+
+    public PersistConfig setFtpHostPortDebug(int ftpHostPortDebug) {
+        this.ftpHostPortDebug = ftpHostPortDebug;
+        return this;
+    }
+
+    public String getFtpUsernameDebug() {
+        return ftpUsernameDebug;
+    }
+
+    public PersistConfig setFtpUsernameDebug(String ftpUsernameDebug) {
+        this.ftpUsernameDebug = ftpUsernameDebug;
+        return this;
+    }
+
+    public String getFtpPasswordDebug() {
+        return ftpPasswordDebug;
+    }
+
+    public PersistConfig setFtpPasswordDebug(String ftpPasswordDebug) {
+        this.ftpPasswordDebug = ftpPasswordDebug;
+        return this;
+    }
+
+    public String getFtpHostNameRelease() {
+        return ftpHostNameRelease;
+    }
+
+    public PersistConfig setFtpHostNameRelease(String ftpHostNameRelease) {
+        this.ftpHostNameRelease = ftpHostNameRelease;
+        return this;
+    }
+
+    public String getFtpHostIPRelease() {
+        return ftpHostIPRelease;
+    }
+
+    public PersistConfig setFtpHostIPRelease(String ftpHostIPRelease) {
+        this.ftpHostIPRelease = ftpHostIPRelease;
+        return this;
+    }
+
+    public int getFtpHostPortRelease() {
+        return ftpHostPortRelease;
+    }
+
+    public PersistConfig setFtpHostPortRelease(int ftpHostPortRelease) {
+        this.ftpHostPortRelease = ftpHostPortRelease;
+        return this;
+    }
+
+    public String getFtpUsernameRelease() {
+        return ftpUsernameRelease;
+    }
+
+    public PersistConfig setFtpUsernameRelease(String ftpUsernameRelease) {
+        this.ftpUsernameRelease = ftpUsernameRelease;
+        return this;
+    }
+
+    public String getFtpPasswordRelease() {
+        return ftpPasswordRelease;
+    }
+
+    public PersistConfig setFtpPasswordRelease(String ftpPasswordRelease) {
+        this.ftpPasswordRelease = ftpPasswordRelease;
+        return this;
     }
 
     public void setAlarmOn(boolean alarmOn) {
@@ -94,7 +398,7 @@ public class PersistConfig extends LitePalSupport {
     }
 
     public String getIp() {
-        if(!TextUtils.isEmpty(newIp)){
+        if (!TextUtils.isEmpty(newIp)) {
             return newIp;
         }
         return ip;
@@ -105,7 +409,7 @@ public class PersistConfig extends LitePalSupport {
     }
 
     public int getPort() {
-        if(newPort != -1){
+        if (newPort != -1) {
             return newPort;
         }
         return port;
@@ -155,32 +459,32 @@ public class PersistConfig extends LitePalSupport {
         this.newPort = newPort;
     }
 
-    public static PersistConfig findConfig(){
+    public static PersistConfig findConfig() {
         PersistConfig persistConfig = LitePal.findFirst(PersistConfig.class);
-        if(persistConfig == null){
+        if (persistConfig == null) {
             persistConfig = new PersistConfig();
             persistConfig.save();
         }
         return persistConfig;
     }
 
-    public static void saveFirstLoginTime(long time){
-        if(TextUtils.isEmpty(findConfig().getFirstLogin())){
-            String tempTime = DateHelper.longToString(time,DateHelper.yyyy_MM_dd_hh_mm_ss);
+    public static void saveFirstLoginTime(long time) {
+        if (TextUtils.isEmpty(findConfig().getFirstLogin())) {
+            String tempTime = DateHelper.longToString(time, DateHelper.yyyy_MM_dd_hh_mm_ss);
             PersistConfig persistConfig = findConfig();
             persistConfig.setFirstLogin(tempTime);
             persistConfig.save();
         }
     }
 
-    public static void saveIsArming(boolean arming){
+    public static void saveIsArming(boolean arming) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setArming(arming);
         persistConfig.save();
     }
 
 
-    public static PersistConfig saveAlarmLedTime(String start,String end){
+    public static PersistConfig saveAlarmLedTime(String start, String end) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setAlarmLedOnTime(start);
         persistConfig.setAlarmLedOffTime(end);
@@ -188,7 +492,7 @@ public class PersistConfig extends LitePalSupport {
         return persistConfig;
     }
 
-    public static PersistConfig saveNewIp(String ip,int port){
+    public static PersistConfig saveNewIp(String ip, int port) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setNewIp(ip);
         persistConfig.setNewPort(port);
@@ -196,60 +500,59 @@ public class PersistConfig extends LitePalSupport {
         return persistConfig;
     }
 
-    public static PersistConfig saveIp(String ip){
+    public static PersistConfig saveIp(String ip) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setIp(ip);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static PersistConfig savePort(int port){
+    public static PersistConfig savePort(int port) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setPort(port);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static PersistConfig saveAlarmNum(String num){
+    public static PersistConfig saveAlarmNum(String num) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setAlarmNum(num);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static PersistConfig saveSpecialNum(String num){
+    public static PersistConfig saveSpecialNum(String num) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setSpecialNum(num);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static PersistConfig saveIsAcceptOtherNum(boolean accept){
+    public static PersistConfig saveIsAcceptOtherNum(boolean accept) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setAcceptOtherNum(accept);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static PersistConfig saveIsIpAlarmFirst(boolean ip){
+    public static PersistConfig saveIsIpAlarmFirst(boolean ip) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setIpAlarmFirst(ip);
         persistConfig.save();
         return persistConfig;
     }
 
-    public static void saveAlarmOn(boolean on){
+    public static void saveAlarmOn(boolean on) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setAlarmOn(on);
         persistConfig.save();
     }
 
-    public static void saveDefaultAlarmOn(boolean on){
+    public static void saveDefaultAlarmOn(boolean on) {
         PersistConfig persistConfig = findConfig();
         persistConfig.setDefaultAlarmOn(on);
         persistConfig.save();
     }
-
 
     @Override
     public String toString() {
@@ -268,6 +571,29 @@ public class PersistConfig extends LitePalSupport {
                 ", isArming=" + isArming +
                 ", isAlarmOn=" + isAlarmOn +
                 ", isDefaultAlarmOn=" + isDefaultAlarmOn +
+                ", ftpHostNameDebugFly='" + ftpHostNameDebugFly + '\'' +
+                ", ftpHostIPDebugFly='" + ftpHostIPDebugFly + '\'' +
+                ", ftpHostPortDebugFly=" + ftpHostPortDebugFly +
+                ", ftpUsernameFly='" + ftpUsernameFly + '\'' +
+                ", ftpPasswordFly='" + ftpPasswordFly + '\'' +
+                ", ftpHostNameDebug='" + ftpHostNameDebug + '\'' +
+                ", ftpHostIPDebug='" + ftpHostIPDebug + '\'' +
+                ", ftpHostPortDebug=" + ftpHostPortDebug +
+                ", ftpUsernameDebug='" + ftpUsernameDebug + '\'' +
+                ", ftpPasswordDebug='" + ftpPasswordDebug + '\'' +
+                ", tcpHostNameDebug1='" + tcpHostNameDebug1 + '\'' +
+                ", tcpHostNameDebug2='" + tcpHostNameDebug2 + '\'' +
+                ", tcpHostIPDebug='" + tcpHostIPDebug + '\'' +
+                ", tcpPortDebug=" + tcpPortDebug +
+                ", ftpHostNameRelease='" + ftpHostNameRelease + '\'' +
+                ", ftpHostIPRelease='" + ftpHostIPRelease + '\'' +
+                ", ftpHostPortRelease=" + ftpHostPortRelease +
+                ", ftpUsernameRelease='" + ftpUsernameRelease + '\'' +
+                ", ftpPasswordRelease='" + ftpPasswordRelease + '\'' +
+                ", tcpHostNameRelease1='" + tcpHostNameRelease1 + '\'' +
+                ", tcpHostNameRelease2='" + tcpHostNameRelease2 + '\'' +
+                ", tcpHostIPRelease='" + tcpHostIPRelease + '\'' +
+                ", tcpPortRelease=" + tcpPortRelease +
                 '}';
     }
 }
