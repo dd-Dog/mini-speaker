6e77b3f4bc750e27c0111548b113b479fa39789b
xingpp <xingpp@flyscale.cn>
2021-02-04 15:19:10 +0800

    完善FileHelper工具类(增加获取文件大小，删除文件，判断文件是否存在)
    Change-Id: I3712cfc1ff95f2e27471c0eb67b08efd5aedeecd

--- app/src/main/java/com/flyscale/alertor/helper/FileHelper.java
+++ app/src/main/java/com/flyscale/alertor/helper/FileHelper.java
@@ -3,9 +3,11 @@ package com.flyscale.alertor.helper;
 import android.os.Environment;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 /**
  * @author 高鹤泉
@@ -22,6 +24,11 @@ public class FileHelper {
     public static final String S_CLIENT_KEY_NAME = "alarmPk8Client.key";
     public static final String S_ROOT_CRT_NAME = "rootCa.crt";
     public static final File S_ALARM_RESP_FILE = new File(getBasePath() + S_ALARM_RESP_NAME);
+
+    public static final int SIZETYPE_B = 1;//获取文件大小单位为B的double值
+    public static final int SIZETYPE_KB = 2;//获取文件大小单位为KB的double值
+    public static final int SIZETYPE_MB = 3;//获取文件大小单位为MB的double值
+    public static final int SIZETYPE_GB = 4;//获取文件大小单位为GB的double值
 
     /**
      * 判断内存卡是否存在
@@ -84,5 +91,126 @@ public class FileHelper {
         }
     }
 
+    /**
+     * 获取指定文件大小
+     * @param file
+     * @return
+     * @throws 　　
+     */
+    public static long getFileSize(File file) throws Exception {
+        long size = 0;
+        if (file.exists()) {
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+        } else {
+            DDLog.e(FileHelper.class , "获取文件大小 , 文件不存在");
+        }
+        return size;
+    }
+
+    /**
+     * 获取指定文件夹大小
+     * @param f
+     * @return
+     * @throws
+     */
+    private static long getFileSizes(File f) throws Exception
+    {
+        long size = 0;
+        File flist[] = f.listFiles();
+        for (int i = 0; i < flist.length; i++){
+            if (flist[i].isDirectory()){
+                size = size + getFileSizes(flist[i]);
+            }
+            else{
+                size =size + getFileSize(flist[i]);
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 转换文件大小,指定转换的类型
+     * @param fileS
+     * @param sizeType
+     * @return
+     */
+    private static String FormetFileSize(long fileS,int sizeType)
+    {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeLong = "";
+        switch (sizeType) {
+            case SIZETYPE_B:
+                fileSizeLong=new DecimalFormat("#").format((double) fileS);
+                break;
+            case SIZETYPE_KB:
+                fileSizeLong=df.format((double) fileS / 1024);
+                break;
+            case SIZETYPE_MB:
+                fileSizeLong=df.format((double) fileS / 1048576);
+                break;
+            case SIZETYPE_GB:
+                fileSizeLong=df.format((double) fileS / 1073741824);
+                break;
+            default:
+                break;
+        }
+        return fileSizeLong;
+    }
+
+    /**
+     * 获取文件指定文件的指定单位的大小
+     * @param filePath 文件路径
+     * @param sizeType 获取大小的类型1为B、2为KB、3为MB、4为GB
+     * @return double值的大小
+     */
+    public static String getFileOrFilesSize(String filePath,int sizeType){
+        File file=new File(filePath);
+        long blockSize=0;
+        try {
+            if(file.isDirectory()){
+                blockSize = getFileSizes(file);
+            }else{
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            DDLog.e(FileHelper.class, "获取文件大小 , 获取失败");
+        }
+        return FormetFileSize(blockSize, sizeType);
+    }
+
+
+    /**
+     * 判断文件是否存在
+     * @param filePath 文件路径
+     */
+    public static boolean fileIsExists(String filePath) {
+        try {
+            File f=new File(filePath);
+            if(!f.exists()) {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 删除单个文件
+     * @param   filePath    被删除文件路径
+     * @return 文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.isFile() && file.exists()) {
+            return file.delete();
+        }
+
+        return false;
+    }
 
 }
