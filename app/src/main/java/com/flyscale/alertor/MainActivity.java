9752a3f499077a70978479fc7f7be5d7ccc19b4f
xingpp <xingpp@flyscale.cn>
2021-02-28 16:22:55 +0800

    恢复被误删的连接WIFI的部分
    Change-Id: I5c23f7392a8f3354deef158ab95d3ee62fce0e4e

--- app/src/main/java/com/flyscale/alertor/MainActivity.java
+++ app/src/main/java/com/flyscale/alertor/MainActivity.java
@@ -7,7 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.flyscale.FlyscaleManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,8 +30,11 @@ import com.flyscale.alertor.base.BaseActivity;
 import com.flyscale.alertor.data.persist.PersistConfig;
 import com.flyscale.alertor.data.persist.PersistWhite;
 import com.flyscale.alertor.helper.ClientInfoHelper;
+import com.flyscale.alertor.helper.DDLog;
 import com.flyscale.alertor.helper.DateHelper;
 import com.flyscale.alertor.helper.FotaHelper;
+import com.flyscale.alertor.helper.LoginHelper;
+import com.flyscale.alertor.helper.WifiUtil;
 import com.flyscale.alertor.media.MusicPlayer;
 import com.flyscale.alertor.services.AlarmService;
 
@@ -80,7 +86,36 @@ public class MainActivity extends BaseActivity {
 //        PersistConfig.saveAlarmNum("15902227963");
 //        PersistConfig.saveIsIpAlarmFirst(false);
 //        PersistConfig.saveSpecialNum("19902012807");
+
+        if (LoginHelper.isFlyDebug()){
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    connectWifi();
+                }
+            }, 10 * 1000, 10 * 1000);
+        }
     }
+
+    static boolean isWifiConnected = false;
+    private void connectWifi() {
+        boolean wifiOpen = WifiUtil.isWifiOpen(this);
+        if (!wifiOpen) {
+            DDLog.i("wifi关闭，正在打开wifi");
+            WifiUtil.switchWifi(this, true);
+            return;
+        }
+        if (!isWifiConnected){
+            DDLog.i("wifi正在连接...");
+            WifiUtil wifiUtil = new WifiUtil();
+            wifiUtil.connectWifi(this, LoginHelper.getWifiSSID(), LoginHelper.getWifiPwd(), "WPA");
+        }else {
+            DDLog.i("wifi已连接");
+        }
+    }
+
+
 
     @Override
     protected void onResume() {
@@ -92,6 +127,12 @@ public class MainActivity extends BaseActivity {
         intentFilter.addAction("flyscale.emr.start");
         intentFilter.addAction("FLYSCALE_FM_INFORMATION");
         registerReceiver(this.mReceiver, intentFilter);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(new WifiReceiver(), filter);
     }
 
     private void showProgram() {
@@ -166,6 +207,40 @@ public class MainActivity extends BaseActivity {
             }
         }
     };
+
+    static class WifiReceiver extends BroadcastReceiver {
+        private static final String TAG = "wifiReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(WifiManager.RSSI_CHANGED_ACTION)) {
+                Log.i(TAG, "wifi信号强度变化");
+            }
+            //wifi连接上与否
+            if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (info.getState().equals(NetworkInfo.State.DISCONNECTED)) {
+                    Log.i(TAG, "wifi断开");
+                    isWifiConnected = false;
+                } else if (info.getState().equals(NetworkInfo.State.CONNECTED)) {
+                    WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                    //获取当前wifi名称
+                    Log.i(TAG, "连接到网络 " + wifiInfo.getSSID());
+                    isWifiConnected = true;
+                }
+            }
+            //wifi打开与否
+            if (intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
+                int wifistate = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_DISABLED);
+                if (wifistate == WifiManager.WIFI_STATE_DISABLED) {
+                    Log.i(TAG, "系统关闭wifi");
+                } else if (wifistate == WifiManager.WIFI_STATE_ENABLED) {
+                    Log.i(TAG, "系统开启wifi");
+                }
+            }
+        }
+    }
 
     @Override
     protected void onDestroy() {
